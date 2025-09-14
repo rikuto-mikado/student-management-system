5cc08bd14d1a0b70b07bc6848d9bf461392fc9c3
Rikuto <mikadorikuto@gmail.com>
2025-09-14 10:53:58 +0900

    add some code

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from PyQt6.QtWidgets import (
     QPushButton,
     QMainWindow,
 )
+from PyQt6.QtGui import QAction
 import sys
 
 
@@ -15,10 +16,18 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle("Student Management System")
 
-        file_manu_item = sse
+        file_menu_item = self.menuBar().addMenu("&File")
+        help_menu_item = self.menuBar().addMenu("&Help")
+
+        add_student_action = QAction("Add Student", self)
+        file_menu_item.addAction(add_student_action)
+
+        about_action = QAction("About", self)
+        help_menu_item.addAction(about_action)
+        about_action.setMenuRole(QAction.MenuRole.NoRole)
 
 
 app = QApplication(sys.argv)
-age_calcurator = MainWindow()
-age_calcurator.show()
+main_window = MainWindow()
+main_window.show()
 sys.exit(app.exec())
